5332f4be9fc123339263a2e301374d4158d0ca3a
tborawski <borawski.3@osu.edu>
2019-03-22 12:38:07 -0400

    Events now need a name before they can be created

--- app/src/main/java/com/example/meetme/ScheduleActivity.java
+++ app/src/main/java/com/example/meetme/ScheduleActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
