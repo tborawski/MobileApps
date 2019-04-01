50671d0f85d4aa9c0aa94e59eb4396b737ff2da0
HPainhas <hpainhas@hotmail.com>
2019-04-01 16:24:24 -0400

    Aspp ask the user if he is sure he actually wants to leave the group.

--- app/src/main/java/com/example/meetme/GroupMainActivity.java
+++ app/src/main/java/com/example/meetme/GroupMainActivity.java
@@ -1,16 +1,19 @@
 package com.example.meetme;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -197,14 +200,30 @@ public class GroupMainActivity extends AppCompatActivity implements View.OnClick
     }
 
     private void leaveGroup() {
-        db.collection("Groups").document(mGroupId).collection("groupUsers").document(mAuth.getCurrentUser().getEmail())
-                .delete().addOnSuccessListener(new OnSuccessListener<Void>() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(GroupMainActivity.this);
+        builder.setTitle("");
+
+        builder.setMessage("Are you sure you wanna leave this group?");
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
             @Override
-            public void onSuccess(Void aVoid) {
-                Intent intent = new Intent(GroupMainActivity.this, MainPageActivity.class);
-                startActivity(intent);
+            public void onClick(DialogInterface dialog, int which) {
+                db.collection("Groups").document(mGroupId).collection("groupUsers").document(mAuth.getCurrentUser().getEmail())
+                        .delete().addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Intent intent = new Intent(GroupMainActivity.this, MainPageActivity.class);
+                        startActivity(intent);
+                    }
+                });
             }
         });
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        builder.create().show();
     }
 
     private void createGroupEvent() {
