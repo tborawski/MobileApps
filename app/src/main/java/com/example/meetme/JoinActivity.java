f73e1d3b17197b5b8ee662b190107db7387f9c58
HPainhas <hpainhas@hotmail.com>
2019-04-04 12:21:35 -0400

    Added searchGroup feature to our Join Activity. Split some code into smaller methods.

--- app/src/main/java/com/example/meetme/JoinActivity.java
+++ app/src/main/java/com/example/meetme/JoinActivity.java
@@ -11,11 +11,13 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -33,6 +35,7 @@ import java.util.Map;
 public class JoinActivity extends AppCompatActivity implements View.OnClickListener {
 
     private ListView mListView;
+    private EditText mFilter;
 
     private ActionBarDrawerToggle mActionBarDrawerToggle;
     private DrawerLayout mDrawerLayout;
@@ -59,77 +62,18 @@ public class JoinActivity extends AppCompatActivity implements View.OnClickListe
         userEmail.setText(mAuth.getCurrentUser().getEmail());
 
         mListView = findViewById(R.id.user_event_listView);
+        mFilter = findViewById(R.id.search_group);
         mDrawerLayout = findViewById(R.id.drawer_layout);
         mToolbar = findViewById(R.id.toolbar);
 
         setSupportActionBar(mToolbar);
 
         setList();
-
-        db.collection("Groups").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                if (task.isSuccessful()) {
-                    for (final QueryDocumentSnapshot document : task.getResult()) {
-                        document.getReference().collection("groupUsers").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                            @Override
-                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                                if (task.isSuccessful()) {
-                                    boolean found = false;
-                                    for (QueryDocumentSnapshot doc : task.getResult()) {
-                                        if (doc.getId().equals(mAuth.getCurrentUser().getEmail())) {
-                                            found = true;
-                                        }
-                                        if (found) {
-                                            userGroups.add(document.getId());
-                                        } else {
-                                            if (document.get("isPrivate").toString().equals("OFF")) {
-                                                groupList.add(document.get("Name").toString());
-                                                docList.add(document.getId());
-                                                mAdapter.notifyDataSetChanged();
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        });
-                    }
-                }
-            }
-        });
-
-
-        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, final int position, long id) {
-                AlertDialog.Builder builder = new AlertDialog.Builder(JoinActivity.this);
-                builder.setTitle("");
-
-                builder.setMessage("Would you like to join this Group?");
-                builder.setPositiveButton("Join", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        Map<String, Object> userJoin = new HashMap<>();
-                        userJoin.put("Level", "Base");
-                        userJoin.put("User", mAuth.getCurrentUser().getEmail());
-                        db.collection("Groups").document(docList.get(position)).collection("groupUsers").document(mAuth.getCurrentUser().getEmail()).set(userJoin);
-                        groupList.remove(position);
-                        docList.remove(position);
-                        mAdapter.notifyDataSetChanged();
-                    }
-                });
-                builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.dismiss();
-                    }
-                });
-                builder.create().show();
-            }
-        });
-
         setActionBarDrawerToggle();
         handleNavigationClickEvents();
+        userAddList();
+        userClickOnItem();
+        searchGroup();
     }
 
     private void handleNavigationClickEvents() {
@@ -166,6 +110,92 @@ public class JoinActivity extends AppCompatActivity implements View.OnClickListe
     private void setActionBarDrawerToggle() {
         mActionBarDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, mToolbar, R.string.drawer_open, R.string.drawer_close);
         mDrawerLayout.addDrawerListener(mActionBarDrawerToggle);
+    }
+
+    private void userAddList() {
+        db.collection("Groups").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    for (final QueryDocumentSnapshot document : task.getResult()) {
+                        document.getReference().collection("groupUsers").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                if (task.isSuccessful()) {
+                                    boolean found = false;
+                                    for (QueryDocumentSnapshot doc : task.getResult()) {
+                                        if (doc.getId().equals(mAuth.getCurrentUser().getEmail())) {
+                                            found = true;
+                                        }
+                                        if (found) {
+                                            userGroups.add(document.getId());
+                                        } else {
+                                            if (document.get("isPrivate").toString().equals("OFF")) {
+                                                groupList.add(document.get("Name").toString());
+                                                docList.add(document.getId());
+                                                mAdapter.notifyDataSetChanged();
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        });
+                    }
+                }
+            }
+        });
+    }
+
+    private void userClickOnItem() {
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, final int position, long id) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(JoinActivity.this);
+                builder.setTitle("");
+
+                builder.setMessage("Would you like to join this Group?");
+                builder.setPositiveButton("Join", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Map<String, Object> userJoin = new HashMap<>();
+                        userJoin.put("Level", "Base");
+                        userJoin.put("User", mAuth.getCurrentUser().getEmail());
+                        db.collection("Groups").document(docList.get(position)).collection("groupUsers")
+                                .document(mAuth.getCurrentUser().getEmail()).set(userJoin);
+                        groupList.remove(position);
+                        docList.remove(position);
+                        mAdapter.notifyDataSetChanged();
+                    }
+                });
+                builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+                builder.create().show();
+            }
+        });
+    }
+
+    private void searchGroup() {
+        mFilter.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                //Do nothing.
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mListView.setAdapter(mAdapter);
+                (JoinActivity.this).mAdapter.getFilter().filter(s);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                //Do nothing.
+            }
+        });
     }
 
     private void setList() {
